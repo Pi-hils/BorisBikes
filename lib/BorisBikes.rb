6c797569c53b10c1ec6b98f63630322217af34b0
Daniel M <danielim@hotmail.co.uk>
2020-07-07 17:29:56 +0100

    Bike specification added

--- lib/BorisBikes.rb
+++ lib/BorisBikes.rb
@@ -1,16 +1,29 @@
 class Bike
+
   def working?
     false
   end
+
 end
 
 class DockingStation
+
   def release_bike
     Bike.new
   end
 
 end
 
+
+# Create new instance of DockingStation class called docking_station
+# This grants access to DockingStation class methods
 docking_station = DockingStation.new
-new_bike = docking_station.release_bike
-p new_bike.working?
+# With the access to DockingStation methods, I can now use release_bike and assign
+# this instance method value to new_bike. This instance creates a new Bike instance
+# (object) of the Bike class. Granting access to the Bike class methods.
+bike = docking_station.release_bike
+# With the access to Bike class methods, I can now call working? method
+bike.working?
+
+# This can also be done in one line. However, you only receive the end value.
+DockingStation.new.release_bike.working?
